28a88502561963e438367f266fc12b550112f7da
abdolrhman <iwcw4i@gmail.com>
2020-03-15 18:20:12 -0400

    addFrontDocker_EditUrlForSearch

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,24 +14,24 @@ export class AppComponent {
   constructor(private httpClient: HttpClient){
     this.httpClient.get("http://localhost:8001/freeTxt?RAM=8").subscribe( response => {
       console.log(response);
-      this.computers = response['data'];    
+      this.computers = response['data'];
     })
   }
 
   searchByMenu(searchValue, searchItem){
-    this.httpClient.get(`http://localhost:8001/freeTxt?${searchItem.value}=${searchValue.value}`).subscribe( response => {
+    this.httpClient.get(`http://localhost:8001?${searchItem.value}=${searchValue.value}`).subscribe( response => {
       console.log(response);
-      this.computers = response['data'];    
+      this.computers = response['data'];
     })
   }
 
   SearchByProduct(searchValue){
     this.computers = [];
     this.httpClient.get(`http://localhost:8001/freeTxt?Product=${searchValue.value}`).subscribe( response => {
-      this.computers = response['data'];    
+      this.computers = response['data'];
     });
     this.httpClient.get(`http://localhost:8001/freeTxt?Company=${searchValue.value}`).subscribe( response => {
-      this.company = response['data'];    
+      this.company = response['data'];
     });
   }
 }
